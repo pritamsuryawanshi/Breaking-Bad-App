47850d069e79bcf8de6ef8ecab4170cfa03b975e
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-07-03 02:15:20 +0530

    ADDED: A new component to search characters by name

---
+++ src/components/ui/Search.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+const Search = (getQuery) => {
+
+    const [text, setText]= useState('')
+    const onchange=(q)=>{
+        setText(q)
+        getQuery(q)
+    }
+    return (
+        <section className={'search'}>
+            <form>
+                <input
+                    type={'text'}
+                    className={'form-control'}
+                    placeholder={`Search by character's name`}
+                    autoFocus
+                    value={text}
+                    onChange={(e)=> onchange(e.target.value)}
+                />
+            </form>
+
+        </section>
+    );
+};
+
+export default Search;
