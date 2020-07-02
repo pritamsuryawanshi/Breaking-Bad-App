9a36511d7ac9fc55f5f42ca199484bfe49498c4a
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-07-03 01:10:44 +0530

    ADDED: Axios to get character details from the API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import Header from "./components/ui/header";
+import * as axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      Welcome to the Breaking Bad App
-    </div>
-  );
+const App = () => {
+    const [items, setItems] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(()=>{
+        const fetchItems= async ()=>{
+            const result= await axios(`https://www.breakingbadapi.com/api/characters`)
+            console.log(result.data);
+        }
+
+        fetchItems()
+    },[])
+    return (
+        <div className="container">
+            <Header/>
+        </div>
+    );
 }
 
 export default App;
