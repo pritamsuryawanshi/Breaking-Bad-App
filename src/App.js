9529708bcbfcf4ff1c64453e080f2becff9c7579
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-07-03 01:26:13 +0530

    ADDED: A component for characters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React,{useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Header from "./components/ui/header";
 import * as axios from "axios";
+import CharacterGrid from "./components/characters/CharacterGrid";
 
 const App = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        const fetchItems= async ()=>{
-            const result= await axios(`https://www.breakingbadapi.com/api/characters`)
+    useEffect(() => {
+        const fetchItems = async () => {
+            const result = await axios(`https://www.breakingbadapi.com/api/characters`)
             console.log(result.data);
+
+            setItems(result.data)
+            setIsLoading(false)
         }
 
         fetchItems()
-    },[])
+    }, [])
     return (
         <div className="container">
             <Header/>
+            <CharacterGrid isLoading={isLoading} items={items}/>
         </div>
     );
 }
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterGrid = ({items, isLoading}) => {
+    return isLoading ? <h1>loading..</h1> : <section className={'cards'}>
+        {items.map(item => (
+            <h1>{item.name}</h1>
+        ))}
+    </section>
+};
+
+export default CharacterGrid;
